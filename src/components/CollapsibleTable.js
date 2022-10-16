6bd39f833411d85b993988f77e84abe0cc756570
Tomasz Wojtyra <t.wojtyra@il-pib.pl>
2022-10-16 15:48:19 +0200

    initial

--- src/components/CollapsibleTable.js
+++ src/components/CollapsibleTable.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Collapse from '@mui/material/Collapse';
@@ -20,29 +21,30 @@ import SearchIcon from '@mui/icons-material/Search';
 import InputWithIcon from "./InputWithIcon";
 import ComboBox from "./ComboBox";
 import InputAdornment from "@mui/material/InputAdornment";
+import axios from 'axios'
 
-function createData(name, calories, fat, carbs, protein, price) {
-    return {
-        name,
-        calories,
-        fat,
-        carbs,
-        protein,
-        price,
-        history: [
-            {
-                date: '2020-01-05',
-                customerId: '11091700',
-                amount: 3,
-            },
-            {
-                date: '2020-01-02',
-                customerId: 'Anonymous',
-                amount: 1,
-            },
-        ],
-    };
-}
+// function createData(name, calories, fat, carbs, protein, price) {
+//     return {
+//         name,
+//         calories,
+//         fat,
+//         carbs,
+//         protein,
+//         price,
+//         history: [
+//             {
+//                 date: '2020-01-05',
+//                 customerId: '11091700',
+//                 amount: 3,
+//             },
+//             {
+//                 date: '2020-01-02',
+//                 customerId: 'Anonymous',
+//                 amount: 1,
+//             },
+//         ],
+//     };
+// }
 
 function Row(props) {
     const {row} = props;
@@ -63,40 +65,42 @@ function Row(props) {
                 <TableCell component="th" scope="row">
                     {row.name}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.salaryFrom}</TableCell>
+                <TableCell align="right">{row.salaryTo}</TableCell>
+                <TableCell align="right">{row.city.name}</TableCell>
+                <TableCell align="right">{row.company.name}</TableCell>
             </TableRow>
             <TableRow>
                 <TableCell style={{paddingBottom: 0, paddingTop: 0}} colSpan={6}>
                     <Collapse in={open} timeout="auto" unmountOnExit>
                         <Box sx={{margin: 1}}>
                             <Typography variant="h6" gutterBottom component="div">
-                                History
+                                Szczegóły ogłoszenia
                             </Typography>
                             <Table size="small" aria-label="purchases">
                                 <TableHead>
                                     <TableRow>
-                                        <TableCell>Date</TableCell>
-                                        <TableCell>Customer</TableCell>
-                                        <TableCell align="right">Amount</TableCell>
-                                        <TableCell align="right">Total price ($)</TableCell>
+                                        <TableCell>Publish date</TableCell>
+                                        <TableCell>Expire date</TableCell>
+                                        <TableCell align="right">Technology</TableCell>
+                                        <TableCell align="right">Seniority</TableCell>
+                                        <TableCell align="right">Description</TableCell>
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {row.history.map((historyRow) => (
-                                        <TableRow key={historyRow.date}>
-                                            <TableCell component="th" scope="row">
-                                                {historyRow.date}
-                                            </TableCell>
-                                            <TableCell>{historyRow.customerId}</TableCell>
-                                            <TableCell align="right">{historyRow.amount}</TableCell>
-                                            <TableCell align="right">
-                                                {Math.round(historyRow.amount * row.price * 100) / 100}
-                                            </TableCell>
-                                        </TableRow>
-                                    ))}
+
+                                    <TableRow>
+                                        <TableCell component="th" scope="row">
+                                            {row.publishDate}
+                                        </TableCell>
+                                        <TableCell>{row.expireDate}</TableCell>
+                                        <TableCell>{row.seniority.name}</TableCell>
+                                        <TableCell align="right">{row.technology.name}</TableCell>
+                                        <TableCell align="right">
+                                            {row.description}
+                                        </TableCell>
+                                    </TableRow>
+
                                 </TableBody>
                             </Table>
                         </Box>
@@ -125,16 +129,51 @@ Row.propTypes = {
     }).isRequired,
 };
 
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-    createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-    createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-    createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-];
+// const rows = [
+//     createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
+//     createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
+//     createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
+//     createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
+//     createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
+// ];
 
 export default function CollapsibleTable() {
     const [filterOpen, setFilterOpen] = React.useState(false);
+    const [data, setData] = React.useState([])
+    const [postData, setPostData] = React.useState([])
+    const get = 'http://localhost:8081/adv/all'
+    const post = 'http://localhost:8081/adv/search?page=0&size=2&sort=id,DESC'
+
+    function createData(name, salaryFrom) {
+        return {
+            name,
+            salaryFrom
+        }
+    }
+
+    const getDataPost = async () => {
+        await axios.post(post, createData("Name co", 10)).then((response) => {
+            const json = response.data
+            console.log(json)
+            setPostData(json)
+        })
+    }
+
+    // const getData = async () => {
+    //     await axios.get(get).then((response) => {
+    //         const json = response.data
+    //         console.log(json)
+    //         setData(json)
+    //     })
+    // }
+
+    // useEffect(() => {
+    //     getData()
+    // }, [])
+
+    useEffect(() => {
+        getDataPost()
+    }, [])
 
     return (
         <>
@@ -154,7 +193,7 @@ export default function CollapsibleTable() {
                                        variant="outlined"/>
                         </Grid>
                         <Grid item sm={9}>
-                            <ComboBox />
+                            <ComboBox/>
                         </Grid>
                         <Grid item sm={1}>
                             <Button color="inherit"
@@ -172,15 +211,15 @@ export default function CollapsibleTable() {
                     <TableHead>
                         <TableRow>
                             <TableCell/>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell align="right">Salary Min</TableCell>
+                            <TableCell align="right">Salary Max</TableCell>
+                            <TableCell align="right">City</TableCell>
+                            <TableCell align="right">Company</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.map((row) => (
+                        {postData.map((row) => (
                             <Row key={row.name} row={row}/>
                         ))}
                     </TableBody>
